fbc23febb631117cefd2e1b2b1c7941f1a5d3aad
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-27 15:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let table = document.getElementById("sampleTable");
-// let insertCount = 1;
+let insertCount = 1;
 
 function insert_Row() {
-      let tr = document.createElement("tr");
-    tr.innerHTML = `
-        <td>New Cell1</td>
-        <td>New Cell2</td>
-    `;
-    table.insertBefore(tr, table.rows[0]);
+      let table = document.getElementById('sampleTable');
+    let newRow = table.insertRow(0); // This inserts a new row at the first position of the table
+
+    let cell1 = newRow.insertCell(0); // This inserts a new cell at the first position of the row
+    let cell2 = newRow.insertCell(1); // This inserts a new cell at the second position of the row
+
+    cell1.innerHTML = 'New Cell1'; // This sets the content of the first cell
+    cell2.innerHTML = 'New Cell2'; // This sets the content of the second cell
 }

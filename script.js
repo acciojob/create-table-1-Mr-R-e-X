fabfbec950ff35967d1f9179e6259e3ff738f08d
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-27 14:53:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 let table = document.getElementById("sampleTable");
-let inserCount = 1;
+// let inserCount = 1;
 function insert_Row() {
   //Write your code here
   let tr = document.createElement("tr");
   tr.innerHTML = `
-                  <td>New Cell${inserCount}</td>
-                  <td>New Cell${inserCount + 1}</td>
+                  <td>New Cell1</td>
+                  <td>New Cell2</td>
                   `;
 table.insertBefore(tr, table.firstChild);
   inserCount++;
